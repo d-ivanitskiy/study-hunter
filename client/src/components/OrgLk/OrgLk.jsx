32d00066cf5b3ddfed9419c442c8db01eab073a1
Zaur <Zulpikarov.Z@gmail.com>
2021-09-02 16:49:53 +0300

    org-tabs-isDone

--- client/src/components/OrgLk/OrgLk.jsx
+++ client/src/components/OrgLk/OrgLk.jsx
@@ -10,6 +10,11 @@ import Modal from '../Modal/Modal';
 function OrgLk() {
   const [file, setFile] = useState(null);
   const [isModalOpened, setIsModalOpened] = useState(false);
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
 
   const { currentOrganization } = useSelector((state) => state);
   useEffect(() => {
@@ -78,43 +83,65 @@ function OrgLk() {
           </div>
         </div>
       </div>
-      <div className="courseInfoPageP2">
-        <h3 style={{ textAlign: 'left' }}>
-          {currentOrganization.is_checked && currentOrganization.is_allowed
-            ? (
-              <div className="addCoursesTitleDiv">
-                {' '}
-                <span>Текущие направления</span>
-                <button onClick={addCourseButtonHandler} type="button" className="btn btn-my-primary">Добавить направление</button>
-              </div>
-            )
-            : <span>Статус регистрации</span>}
-        </h3>
-        {isModalOpened ? (
-          <Modal setIsModalOpened={setIsModalOpened} orgId={currentOrganization.id} />
-        ) : null}
-        <hr style={{ marginTop: 0 }} />
-        {currentOrganization.is_allowed
-          ? (
-            <div>
-              <div style={{ marginLeft: '30px' }}>
-                {Object.keys(currentOrganization).length
-                  ? currentOrganization.OrganizationCourses.map((course) => (
-                    <CoursesTable
-                      key={course.id}
-                      courseName={course.name}
-                      coursePrice={course.price}
-                      courseId={course.id}
-                    />
-                  )) : 'Здесь пока ничего нет' }
-              </div>
-              <div>
-                <div>Отклики пользователей</div>
-                <hr />
-              </div>
+
+      <div className="tabs">
+        <nav>
+          <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <button className={toggleState === 1 ? 'nav-link active' : 'nav-link'} onClick={() => toggleTab(1)} id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Главная</button>
+            <button className={toggleState === 2 ? 'nav-link active' : 'nav-link'} onClick={() => toggleTab(2)} id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Профиль</button>
+          </div>
+        </nav>
+        <div className="tab-content" id="nav-tabContent">
+          <div className={toggleState === 1 ? 'tab-pane fade show active' : 'tab-pane fade'} id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+            <div className="courseInfoPageP2">
+              <h3 style={{ textAlign: 'left' }}>
+                {currentOrganization.is_checked && currentOrganization.is_allowed
+                  ? (
+                    <div className="addCoursesTitleDiv">
+                      {' '}
+                      <span>Текущие направления</span>
+                      <button onClick={addCourseButtonHandler} type="button" className="btn btn-my-primary">Добавить направление</button>
+                    </div>
+                  )
+                  : <span>Статус регистрации</span>}
+              </h3>
+              {isModalOpened ? (
+                <Modal setIsModalOpened={setIsModalOpened} orgId={currentOrganization.id} />
+              ) : null}
+              <hr style={{ marginTop: 0 }} />
             </div>
-          ) : currentOrganization.is_checked ? 'Заявка отклонена модератором' : 'Заявка на регистрацию на рассмотрении у модератора'}
+            {currentOrganization.is_allowed
+              ? (
+                <div>
+                  <div style={{ marginLeft: '30px' }}>
+                    {Object.keys(currentOrganization).length
+                      ? currentOrganization.OrganizationCourses.map((course) => (
+                        <CoursesTable
+                          key={course.id}
+                          courseName={course.name}
+                          coursePrice={course.price}
+                          courseId={course.id}
+                        />
+                      )) : 'Здесь пока ничего нет' }
+                  </div>
+                </div>
+              ) : currentOrganization.is_checked ? 'Заявка отклонена модератором' : 'Заявка на регистрацию на рассмотрении у модератора'}
+          </div>
+
+          <div className={toggleState === 2 ? 'tab-pane fade show active' : 'tab-pane fade'} id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+            {currentOrganization.is_allowed
+              ? (
+                <div>
+                  <div>
+                    <div>Отклики пользователей</div>
+                    <hr />
+                  </div>
+                </div>
+              ) : currentOrganization.is_checked ? 'Заявка отклонена модератором' : 'Заявка на регистрацию на рассмотрении у модератора'}
+          </div>
+        </div>
       </div>
+
     </div>
   );
 }
