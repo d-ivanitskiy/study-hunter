fc09a2f7a4abbd4424b8afe113bb1d7dfd512f41
Zaur <Zulpikarov.Z@gmail.com>
2021-09-02 18:01:03 +0300

    tabs for superuser

--- client/src/components/Lk/Lk.jsx
+++ client/src/components/Lk/Lk.jsx
@@ -15,6 +15,12 @@ export default function Lk() {
   const currentUser = useSelector((state) => state.currentUser);
   const [currentFavorites, setCurrentFavorites] = useState([]);
   const [isUser, setIsUser] = useState(false);
+  const [toggleState, setToggleState] = useState(1);
+
+  const toggleTab = (index) => {
+    setToggleState(index);
+  };
+
   useEffect(() => {
     setCurrentFavorites(currentUser.favorites);
   }, [isUser]);
@@ -67,24 +73,49 @@ export default function Lk() {
           <p>{currentUser.email}</p>
         </div>
       </div>
-      {currentUser.superadmin ? (
-        <div>
-          <div className="courseInfoPageP2">
-            { currentUser.admin ? <RegisterList /> : ''}
-            <h3 style={{ textAlign: 'left', marginTop: '20px' }}>Администраторы</h3>
-            <hr style={{ marginTop: 0 }} />
-            <div>
-              <UserList />
-            </div>
+
+      <div className="tabs">
+        <nav>
+          <div className="nav nav-tabs" id="nav-tab" role="tablist">
+            <button className={toggleState === 1 ? 'nav-link active' : 'nav-link'} onClick={() => toggleTab(1)} id="nav-home-tab" data-bs-toggle="tab" data-bs-target="#nav-home" type="button" role="tab" aria-controls="nav-home" aria-selected="true">Заявки УЗ</button>
+            <button className={toggleState === 3 ? 'nav-link active' : 'nav-link'} onClick={() => toggleTab(3)} id="nav-contact-tab" data-bs-toggle="tab" data-bs-target="#nav-contact" type="button" role="tab" aria-controls="nav-contact" aria-selected="false">Администраторы</button>
+            <button className={toggleState === 2 ? 'nav-link active' : 'nav-link'} onClick={() => toggleTab(2)} id="nav-profile-tab" data-bs-toggle="tab" data-bs-target="#nav-profile" type="button" role="tab" aria-controls="nav-profile" aria-selected="false">Поиск пользователей</button>
           </div>
-          <div className="courseInfoPageP2">
-            <h3 style={{ textAlign: 'left', marginTop: '20px' }}>Поиск пользователей</h3>
-            <hr style={{ marginTop: 0 }} />
-            <div>
-              <UserSearch />
-            </div>
-          </div>
+        </nav>
+        <div className="tab-content" id="nav-tabContent">
+          {currentUser.superadmin ? (
+            <>
+              <div className={toggleState === 1 ? 'tab-pane fade show active' : 'tab-pane fade'} id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+                <div className="courseInfoPageP2">
+                  { currentUser.admin ? <RegisterList /> : ''}
+                </div>
+              </div>
+              <div className={toggleState === 2 ? 'tab-pane fade show active' : 'tab-pane fade'} id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+                <div className="courseInfoPageP2">
+                  <h3 style={{ textAlign: 'left', marginTop: '20px' }}>Поиск пользователей</h3>
+                  <hr style={{ marginTop: 0 }} />
+                  <div>
+                    <UserSearch />
+                  </div>
+                </div>
+              </div>
+              <div className={toggleState === 3 ? 'tab-pane fade show active' : 'tab-pane fade'} id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+                <div className="courseInfoPageP2">
+                  <h3 style={{ textAlign: 'left', marginTop: '20px' }}>Администраторы</h3>
+                  <hr style={{ marginTop: 0 }} />
+                  <div>
+                    <UserList />
+                  </div>
+                </div>
+              </div>
+            </>
+          ) : null}
+
         </div>
+      </div>
+
+      {currentUser.superadmin ? (
+        <div />
       ) : (
         <>
           <div className="courseInfoPageP2">
@@ -103,7 +134,7 @@ export default function Lk() {
                     </Link>
                   </div>
                 )
-            ) : null }
+            ) : null}
           </div>
           <div className="courseInfoPageP2">
             <h3 style={{ textAlign: 'left' }}>Отклики</h3>
@@ -121,11 +152,12 @@ export default function Lk() {
                     </Link>
                   </div>
                 )
-            ) : null }
-            { currentUser.admin ? <RegisterList /> : ''}
+            ) : null}
+            {currentUser.admin ? <RegisterList /> : ''}
           </div>
         </>
       )}
+
     </div>
   );
 }
